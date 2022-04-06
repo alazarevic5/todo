e94a581615107bad42853d3646622a8d2f6d9026
Gupa 1 <gupa1@Administrators-iMac-2.local>
2022-04-06 20:45:06 +0200

    rad na aktivnim stavkama

--- ToDo/ToDo/Controller/ActiveViewController.swift
+++ ToDo/ToDo/Controller/ActiveViewController.swift
@@ -19,17 +19,55 @@ class ActiveCell: UITableViewCell {
     @IBOutlet weak var backgroundViewOfItem: UIView! // Ovaj view dodat je iza svih ovih kontrola kako bi mogao da se napravi efekat okvira oko kontrola unutar ćelije
 }
 
-class ActiveViewController: UIViewController {
+class ActiveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!   // table view
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!   // indikator za učitavanje (loader) može biti prikazan ili sakriven (prikazuje se dok se učitavaju podaci, a sakriva kada se prikažu podaci)
     
+    var aktivneStavke = [ToDoItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getActiveItems()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return aktivneStavke.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celija = tableView.dequeueReusableCell(withIdentifier: "activeCell") as! ActiveCell
+        
+        // popunjavanje podataka
+        
+        return celija
+    }
+    
+    func getActiveItems() {
+        
+        ToDoRequests.getAllItems { rezulatati in
+            
+            let results = rezulatati["results"] as? [[String:Any]]
+            if let results = results {
+                for res in results {
+                    print(res)
+
+                    let stavka = ToDoItem(id: res["id"] as! Int, title: res["title"] as! String, content: res["content"] as! String, isCompleted: res["isCompleted"] as! Int, priority: res["priority"] as! Int, date: res["date"] as! String)
+                    
+                    if stavka.isCompleted == 0 {
+                        self.aktivneStavke.append(stavka)
+                        
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                            self.activityIndicator.stopAnimating()
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
 
