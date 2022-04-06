56316bb8deda29361b99ee43efe496e55772d97c
Aleksa <aleksa@Aleksas-MacBook-Air.local>
2022-04-06 20:45:03 +0200

    svasta smo menjali

---
+++ ToDo/ToDo/Model/Requests.swift
@@ -0,0 +1,31 @@
+//
+//  Requests.swift
+//  ToDo
+//
+//  Created by Aleksa on 6.4.22..
+//
+
+import Foundation
+import UIKit
+
+class ToDoRequests {
+    
+    static var baseURL: String = "http://www.xcode.rs:8000/"
+    static var uuid: String = "123456"
+    
+    static func getAllItems(rezultati: @escaping ([String : Any]) -> Void) {
+        // pravljenje url-a
+        
+        let url = URL(string: baseURL + "all/" + uuid)!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let data = data {
+                if let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] {
+                    rezultati(jsonData)
+                }
+            }
+    }
+        task.resume()
+}
+}
