75338fafc98c23933f1c35ad805571c6428528fd
Dunja Savic <dunja.savic@Dunjas-MacBook-Air.local>
2022-04-10 17:41:21 +0200

    svi moji pokusaji

---
+++ ToDo/ToDo/ProbaDunja.swift
@@ -0,0 +1,11 @@
+//
+//  ProbaDunja.swift
+//  ToDo
+//
+//  Created by Dunja Savic on 10.4.22..
+//
+
+import Foundation
+struct DS {
+    var poruka: String = "Dunja je"
+}
---
+++ ToDo/ToDo/proba DS.swift
@@ -0,0 +1,8 @@
+//
+//  proba DS.swift
+//  ToDo
+//
+//  Created by Dunja Savic on 10.4.22..
+//
+
+import Foundation
